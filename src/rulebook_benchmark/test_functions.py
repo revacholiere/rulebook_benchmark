fba98e94ef1a17b74a01f8fdc5045cff0040d7ce
kevinchang <kaichunchang@berkeley.edu>
2025-04-15 18:16:21 -0700

    add the rulebook class and examples

---
+++ src/rulebook_benchmark/test_functions.py
@@ -0,0 +1,55 @@
+def test_func_0(traj):
+    if traj == 'a':
+        return 2
+    elif traj == 'b':
+        return 1
+    else:
+        raise ValueError('Invalid trajectory')
+    
+def test_func_1(traj):
+    if traj == 'a':
+        return 1
+    elif traj == 'b':
+        return 2
+    else:
+        raise ValueError('Invalid trajectory')
+    
+def test_func_2(traj):
+    if traj == 'a':
+        return 1
+    elif traj == 'b':
+        return 1
+    else:
+        raise ValueError('Invalid trajectory')
+    
+def test_func_3(traj):
+    if traj == 'a':
+        return 2
+    elif traj == 'b':
+        return 1
+    else:
+        raise ValueError('Invalid trajectory')
+    
+def test_func_4(traj):
+    if traj == 'a':
+        return 1
+    elif traj == 'b':
+        return 2
+    else:
+        raise ValueError('Invalid trajectory')
+
+def test_func_5(traj):
+    if traj == 'a':
+        return 2
+    elif traj == 'b':
+        return 2
+    else:    
+        raise ValueError('Invalid trajectory')
+    
+def test_func_6(traj):
+    if traj == 'a':
+        return 2
+    elif traj == 'b':
+        return 1
+    else:
+        raise ValueError('Invalid trajectory')
